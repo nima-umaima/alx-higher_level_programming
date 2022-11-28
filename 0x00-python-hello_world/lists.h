87995fe4038082c7d6c3500473c05d7733ad183f
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-11-28 19:19:07 +0300

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,11 +2,11 @@
 #define LISTS_H
 #include <stddef.h>
 /**
-*
-*
-*
-*
-*
+* struct listint_s - singly linked list
+*@n: integer
+*@next: points to the next node
+* Description: singly linked list node structure
+* for Holberton project
 */
 typedef struct listint_s
 {
