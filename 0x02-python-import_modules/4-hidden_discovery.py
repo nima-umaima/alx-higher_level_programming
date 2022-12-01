8dc3a1adea902893a30d64013c855ad58291c269
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-12-01 23:31:50 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if not i.startswith("__"):
-            print("{:s}".format(i))
+    for word in dir(hidden_4):
+        if word[:2] != "__":
+            print("{:s}".format(word))
