daf70205dcbf6f1d2275c90e44fca79c79436eb5
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-12-05 15:41:54 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if id < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return (my_list)
     else:
-        listix = my-list[:]
+        listix = my_list[:]
         listix[idx] = element
         return(listix)
