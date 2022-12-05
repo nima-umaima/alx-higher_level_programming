e512d3a50682c39c63b070e79be844ead0170d00
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-12-05 18:33:02 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    my_list = list(my_string)
-    for ch in my_list:
-        if ch == 'c' or ch == 'C':
-            del my_list[i]
-        i = i + 1
-    return ''.join(my_list)
+    rem = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            print("{:s}".format(i), end="")
+    return rem
