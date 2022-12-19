5fc313df7da663de740033e2c82627156f69f9eb
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-12-19 19:24:26 +0300

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return (False)
+    else:
+        return (True)
