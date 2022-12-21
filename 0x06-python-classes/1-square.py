b72bc23dd668c981f1e323cedcad25eb2879dac0
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-12-21 04:32:04 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
-#!
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square, """
+        self.__size = size
