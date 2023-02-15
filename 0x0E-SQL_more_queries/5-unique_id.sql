e3ada336055cbd59522f341dc42acdddbc800205
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-02-15 15:46:11 +0300

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,8 @@
-#11;rgb:0000/0000/0000
+-- creates the table unique_id
+-- description of table data: id INT with the default value 1, must be unique & name VARCHAR(256)
+-- should not fail if exists
+
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
+       name VARCHAR(256)
+);
