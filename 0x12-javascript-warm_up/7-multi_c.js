f3552f1061117f4058e6d0c149d4b2cf72bf1103
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-03-14 17:52:53 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2], 10);
-if (isNaN(num)) {
-    console.log('Missing number of occurences');
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = num; i > 0; i -= 1) {
-	console.log('C is fun');
-    }
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
