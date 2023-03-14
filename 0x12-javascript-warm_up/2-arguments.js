a7cc6c4efc159760c357c1783f92122fdf3bf780
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-03-14 17:49:29 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-const numberArguments = process.argv.length;
-if (numberArguments === 2) console.log('No argument');
-else if (numberArguments === 3) console.log('Argument found');
-else console.log('Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
