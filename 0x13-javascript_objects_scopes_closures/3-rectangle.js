e2b86c1af78d3804aac9106e922dc0ac67802b88
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-03-14 18:13:39 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let col = 0; col < this.height; col += 1) {
-      console.log('X'.repeat(this.width));
+    let toPrint = '';
+    for (let i = 0; i < this.width; i++) {
+      toPrint += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(toPrint);
     }
   }
-};
+}
+module.exports = Rectangle;
