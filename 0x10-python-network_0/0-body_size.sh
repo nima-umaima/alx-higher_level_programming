1f6285f8b600c93b5fb1a11faeec1b0c44598553
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-03-31 05:13:36 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+# takes in a URL, sends a requesto to that URL and displays the size of the body of the response
+curl -sI "$1" | grep Content-Length | cut -d " " -f2
