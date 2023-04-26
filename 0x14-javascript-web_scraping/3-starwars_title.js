cc27e9eb3f7d14804c71d345a7ed2e7ebc8a2d23
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-04-26 15:18:25 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
+
+let id = process.argv[2];
+let url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body['title']);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
 });
